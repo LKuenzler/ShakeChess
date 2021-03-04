26353c60c7bea31fedc5df04a7d72da7ada96395
Laurin Künzler <lauknz@az.zli.ch>
2021-03-04 08:52:42 +0100

    Added game activity

--- app/src/main/java/ch/laurin/shakechess/activities/MainActivity.java
+++ app/src/main/java/ch/laurin/shakechess/activities/MainActivity.java
@@ -2,15 +2,28 @@ package ch.laurin.shakechess.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import ch.laurin.shakechess.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startButton;
+    private Button resumeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton = (Button) findViewById(R.id.startbutton);
+        resumeButton = (Button) findViewById(R.id.resumebutton);
+        startButton.setOnClickListener(v ->  {
+            Intent myIntent = new Intent(MainActivity.this, GameActivity.class);
+            MainActivity.this.startActivity(myIntent);
+        });
     }
+
+
+
 }

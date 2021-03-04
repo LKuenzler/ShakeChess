314528699d12d8e9b255d49911fe05687e400513
Laurin Künzler <lauknz@az.zli.ch>
2021-03-04 08:38:38 +0100

    First commit

---
+++ app/src/main/java/ch/laurin/shakechess/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package ch.laurin.shakechess.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.laurin.shakechess.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
